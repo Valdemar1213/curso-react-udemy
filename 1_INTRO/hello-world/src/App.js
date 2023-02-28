1b5eca0073575953d19e1d11b3386ed694e58bd7
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-02-28 20:42:41 -0300

    Exercício seção 1

--- 1_INTRO/hello-world/src/App.js
+++ 1_INTRO/hello-world/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World React</h1>
+      <h2>Iniciando o curso React da plataforma Udemy, do instrutor Matheus Battisti</h2>
     </div>
   );
 }
